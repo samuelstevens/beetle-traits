c8292a96be57af986801234b41ab9b102b96ae9c
Samuel Stevens <samuel.robert.stevens@gmail.com>
2025-08-13 14:19:07 -0400

    add other dataset types

--- src/btx/data/__init__.py
+++ src/btx/data/__init__.py
@@ -1,4 +1,15 @@
+from .biorepo import Config as BiorepoConfig
+from .biorepo import Dataset as BiorepoDataset
 from .hawaii import Config as HawaiiConfig
 from .hawaii import Dataset as HawaiiDataset
+from .neon import Config as NeonConfig
+from .neon import Dataset as NeonDataset
 
-__all__ = ["HawaiiConfig", "HawaiiDataset"]
+__all__ = [
+    "BiorepoConfig",
+    "BiorepoDataset",
+    "HawaiiConfig",
+    "HawaiiDataset",
+    "NeonConfig",
+    "NeonDataset",
+]
